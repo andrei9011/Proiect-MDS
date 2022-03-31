ea7c763feb1c9b7c9949e61b916afb6bd9e70dc4
Iliescu Andrei <63337452+andrei9011@users.noreply.github.com>
2022-03-31 19:49:17 +0300

    Add files via upload

--- Proiect_MDS.cs
+++ Proiect_MDS.cs
@@ -25,7 +25,51 @@ class Program{
 
     public class BlockChain : IEnumerable<IBlock>
     {
-        
+        private List<IBlock> _items = new List<IBlock>();
+
+        public BlockChain(byte[] difficulty, IBlock genesis)
+        {
+            Difficulty = difficulty;
+            genesis.Hash = genesis.MineHas(difficulty);
+            Items.add(genesis);
+
+        }
+
+
+        public void Add(IBlock item)
+        {
+            if(Items.LastOrDefault() != null)
+            {
+                item.PrevHash = Items.LastOrDefault()?.Hash;
+            }
+            item.Hash = item.MineHas(Difficulty);
+            Items.Add(item);
+        }
+
+        public int Count => Items.Count;
+
+        public IBlock thisp[index]{
+            get => Items[index];
+            set => Items[index] = value;
+        }
+
+        public List<IBlock> Items
+        {
+            get => _items;
+            set => _items = value;
+        }
+
+        public byte[] Difficulty { get; }
+
+        public IEnumerator<IBlock> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return  Items.GetEnumerator();
+        }
     }
 
     static void Main(string[] args)
